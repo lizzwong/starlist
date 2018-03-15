b637918b89a36118855a1207ba81062f819361e0
Elizabeth Wong <lizzwong@gmail.com>
2018-03-15 09:45:26 -0500

    woo starlist

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+
+constructor(props){
+  super(props);
+
+  this.state = { starList: ['Gacrux ', 'Hadar ', 'Fomalhaut '] }
+
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+         {this.state.starList}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
